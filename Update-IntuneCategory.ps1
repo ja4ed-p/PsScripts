97bd12047774ab65dda9ce54dfa0a09707168994
ja4ed-p <ja4ed.p@gmail.com>
2025-10-17 20:01:05 -0600

    Add files via upload

---
+++ Update-IntuneCategory.ps1
@@ -0,0 +1,49 @@
+﻿#Define ID and variables for MS Graph connection
+$tenantId="[YOUR-TENANT-ID]"
+$clientId="[YOUR-CLIENT-ID]"
+
+#Connect to MS Graph
+Connect-MgGraph -TenantId $TenantId -ClientId $ClientId
+
+#Get all ACC devices in Intune
+$allDevices = Get-MgDeviceManagementManagedDevice -All
+
+#Filter devices with no category assigned
+$unassignedDevices = $allDevices | Where-Object {$_.DeviceCategoryDisplayName -ne "[YOUR-CATEGORY-NAME]"}
+
+#Preview results if desired
+$unassignedDevices | Select-Object Id,DeviceName,OperatingSystem,DeviceCategoryDisplayName | Out-GridView
+
+#Define the ACC Devices category Id
+$deviceCategoryId = "[YOUR-CATEGORY-ID]"
+
+#Define headers for the Invoke-MgGraphRequest below
+$headers = @{
+    "Content-Type" = "application/json"
+}
+
+foreach ($device in $unassignedDevices) {
+    $deviceId = $device.Id
+
+    #Construct the JSON body
+    $body = @{
+        '@odata.id' = "https://graph.microsoft.com/beta/deviceManagement/deviceCategories/$deviceCategoryId"
+    } | ConvertTo-Json -Depth 3
+
+    #Send the PUT request to assign the category
+    try{
+        Write-Output "Assigning category to device: $($device.DeviceName)"
+
+        Invoke-MgGraphRequest -Method PUT `
+            -Uri "https://graph.microsoft.com/beta/deviceManagement/managedDevices/$deviceId/deviceCategory/`$ref" `
+            -Headers $headers `
+            -Body $body
+    }
+
+    catch{
+        Write-Warning "Failed to assign category to device: $($device.Devicename)"
+    }
+}
+
+#Disconnect from MS Graph
+Disconnect-MgGraph
